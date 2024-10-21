483d0c91af024dec80cc541a4f91bc128b5cef16
Jeff Herrera <jeffherrera@jeffs-mac-mini.lan>
2024-10-21 18:17:27 -0400

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [astroImageTools],
+});
